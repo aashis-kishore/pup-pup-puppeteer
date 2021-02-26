4c1749489c74afb47a46b358c1a2e6a45bb23f5d
aashis-kishore <aashiskishore@gmail.com>
2021-02-26 16:30:57 +0530

    Update app

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -8,8 +8,28 @@ const index = (req, res, next) => {
   return res.status(200).sendFile('index.html', { root })
 }
 
-const about = (req, res, next) => {
-  return res.status(200).sendFile('about.html', { root })
+const contact = (req, res, next) => {
+  return res.status(200).sendFile('contact.html', { root })
 }
 
-export default { index, about }
+const onContact = (req, res, next) => {
+  console.info('Contact form submitted successfully')
+  req.app.formSubmitted = true
+
+  console.info('E-mail:', req.body.email)
+  console.info('Subscription to Newsletter:', req.body.subToNewsletter)
+
+  return res.redirect('/contact-status')
+}
+
+const contactStatus = (req, res, next) => {
+  if (!req.app.formSubmitted) {
+    req.app.formSubmitted = false
+    return res.redirect('/contact')
+  }
+
+  req.app.formSubmitted = false
+  return res.status(200).sendFile('contactStatus.html', { root })
+}
+
+export default { index, contact, onContact, contactStatus }
--- app/routes/index.route.js
+++ app/routes/index.route.js
@@ -3,9 +3,14 @@ import express from 'express'
 import IndexController from '../controllers/index.controller.js'
 
 const router = express.Router()
+const urlEncodedParser = express.urlencoded({ extended: false })
 
 router.get('', IndexController.index)
 
-router.get('/about', IndexController.about)
+router.route('/contact')
+  .get(IndexController.contact)
+  .post(urlEncodedParser, IndexController.onContact)
+
+router.get('/contact-status', IndexController.contactStatus)
 
 export default router
